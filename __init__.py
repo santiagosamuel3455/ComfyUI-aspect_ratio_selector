a4beaefad3a6a261098382a227772424294ddf5b
santiagosamuel3455 <santiagosamuel3455@gmail.com>
2025-05-16 23:28:58 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "AspectRatioSelectorNode": "Aspect Ratio Selector Node"
 }
 
-WEB_DIRECTORY = "./web"  # opcional, si usas UI adicional
-__version__ = "1.0.0"
+#WEB_DIRECTORY = "./web"  # opcional, si usas UI adicional
+__version__ = "1.2.0"
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
